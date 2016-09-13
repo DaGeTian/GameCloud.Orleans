612f4c310810638ad739e1409905b7688a737052
shuai.chen <shuaichen_ax@163.com>
2016-09-13 18:41:25 +0800

    delete XUPorter add simple code to add framework and libs into xcode

--- GameCloud.Unity.Client/Assets/GameCloud.Unity.Internal/Editor/GameCloudEditorInternal.cs
+++ GameCloud.Unity.Client/Assets/GameCloud.Unity.Internal/Editor/GameCloudEditorInternal.cs
@@ -14,11 +14,12 @@ public class GameCloudEditorInternal : EditorWindow
     [MenuItem("GameCloud.Unity/Native/GameCloud.Unity.NativeAll")]
     static void exportGameCloudUnityNativePackageAll()
     {
-        string[] arr_assetpathname = new string[3];
+        string[] arr_assetpathname = new string[5];
         arr_assetpathname[0] = "Assets/GameCloud.Unity.Native";
-        arr_assetpathname[1] = "Assets/Plugins/Android";
-        arr_assetpathname[2] = "Assets/Plugins/GameCloud.Unity.Native";        
-        arr_assetpathname[2] = "Assets/GameCloud.Unity.Sample/Native";
+        arr_assetpathname[1] = "Assets/GameCloud.Unity.Client/Editor/XcodeProjectMod.cs";
+        arr_assetpathname[2] = "Assets/Plugins/Android";
+        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native";        
+        arr_assetpathname[4] = "Assets/GameCloud.Unity.Sample/Native";
 
         AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "GameCloud.Unity.NativeAll.unitypackage", ExportPackageOptions.Recurse);
 
@@ -29,11 +30,12 @@ public class GameCloudEditorInternal : EditorWindow
     [MenuItem("GameCloud.Unity/Native/导出GameCloud.Unity.NativeDataEye")]
     static void exportGameCloudUnityNativeDataEye()
     {
-        string[] arr_assetpathname = new string[4];
+        string[] arr_assetpathname = new string[5];
         arr_assetpathname[0] = "Assets/GameCloud.Unity.Native";
-        arr_assetpathname[1] = "Assets/Plugins/Android";
-        arr_assetpathname[2] = "Assets/Plugins/GameCloud.Unity.Native/DataEye";
-        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
+        arr_assetpathname[1] = "Assets/GameCloud.Unity.Client/Editor/XcodeProjectMod.cs";
+        arr_assetpathname[2] = "Assets/Plugins/Android";
+        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/DataEye";
+        arr_assetpathname[4] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
         AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "GameCloud.Unity.NativeDataEye.unitypackage", ExportPackageOptions.Recurse);
 
         Debug.Log("Export GameCloud.Unity.NativeDataEye.unitypackage Finished!");
@@ -43,11 +45,12 @@ public class GameCloudEditorInternal : EditorWindow
     [MenuItem("GameCloud.Unity/Native/导出GameCloud.Unity.NativePay")]
     static void exportUnityNativePay()
     {
-        string[] arr_assetpathname = new string[4];
+        string[] arr_assetpathname = new string[5];
         arr_assetpathname[0] = "Assets/GameCloud.Unity.Native";
-        arr_assetpathname[1] = "Assets/Plugins/Android";
-        arr_assetpathname[2] = "Assets/Plugins/GameCloud.Unity.Native/Pay";
-        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
+        arr_assetpathname[1] = "Assets/GameCloud.Unity.Client/Editor/XcodeProjectMod.cs";
+        arr_assetpathname[2] = "Assets/Plugins/Android";
+        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/Pay";
+        arr_assetpathname[4] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
         AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "GameCloud.Unity.NativePay.unitypackage", ExportPackageOptions.Recurse);
 
         Debug.Log("Export GameCloud.Unity.NativePay.unitypackage Finished!");
@@ -57,11 +60,12 @@ public class GameCloudEditorInternal : EditorWindow
     [MenuItem("GameCloud.Unity/Native/导出GameCloud.Unity.Native")]
     static void exportGameCloudNativeBySelf()
     {
-        string[] arr_assetpathname = new string[4];
+        string[] arr_assetpathname = new string[5];
         arr_assetpathname[0] = "Assets/GameCloud.Unity.Native";
-        arr_assetpathname[1] = "Assets/Plugins/Android";
-        arr_assetpathname[2] = "Assets/Plugins/GameCloud.Unity.Native/Native";
-        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
+        arr_assetpathname[1] = "Assets/GameCloud.Unity.Client/Editor/XcodeProjectMod.cs";
+        arr_assetpathname[2] = "Assets/Plugins/Android";
+        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/Native";
+        arr_assetpathname[4] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
         AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "GameCloud.Unity.Native.unitypackage", ExportPackageOptions.Recurse);
 
         Debug.Log("Export GameCloud.Unity.Native.unitypackage Finished!");
@@ -71,11 +75,12 @@ public class GameCloudEditorInternal : EditorWindow
     [MenuItem("GameCloud.Unity/Native/导出GameCloud.Unity.Speech")]
     static void exportBaiduSpeech()
     {
-        string[] arr_assetpathname = new string[4];
+        string[] arr_assetpathname = new string[5];
         arr_assetpathname[0] = "Assets/GameCloud.Unity.Native";
-        arr_assetpathname[1] = "Assets/Plugins/Android";
-        arr_assetpathname[2] = "Assets/Plugins/GameCloud.Unity.Native/Speech";
-        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
+        arr_assetpathname[1] = "Assets/GameCloud.Unity.Client/Editor/XcodeProjectMod.cs";
+        arr_assetpathname[2] = "Assets/Plugins/Android";
+        arr_assetpathname[3] = "Assets/Plugins/GameCloud.Unity.Native/Speech";
+        arr_assetpathname[4] = "Assets/Plugins/GameCloud.Unity.Native/SDKReceiveMono";
         AssetDatabase.ExportPackage(arr_assetpathname, mUnityPackagePath + "GameCloud.Unity.Speech.unitypackage", ExportPackageOptions.Recurse);
 
         Debug.Log("Export GameCloud.Unity.Speech.unitypackage Finished!");
