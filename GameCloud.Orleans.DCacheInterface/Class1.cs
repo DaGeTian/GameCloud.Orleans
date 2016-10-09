5fd620f449766c8a2d26595a08c75a1fb4c560ba
lion <xiaolige945@vip.qq.com>
2016-10-09 22:19:41 +0800

    添加GameCloud.Orleans.DCache

---
+++ GameCloud.Orleans.DCacheInterface/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCloud.Orleans.DCacheInterface
+{
+    public class Class1
+    {
+    }
+}
