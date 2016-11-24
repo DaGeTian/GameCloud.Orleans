6bafb6b02ff8fa6ee789499c643555a10edef952
xiaolige945@vip.qq.com <xiaolige945@vip.qq.com>
2016-11-24 16:01:53 +0800

    GrainEx代码整理。

--- GameCloud.Orleans.GrainEx/Properties/AssemblyInfo.cs
+++ GameCloud.Orleans.GrainEx/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31c66a07-18c6-4548-b71d-35a6265fdb2d")]
+[assembly: Guid("1de832df-27a2-4a1a-a686-a2ce68f4f8ee")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.15.0")]
-[assembly: AssemblyFileVersion("1.0.15.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- NuGet/GameCloud.Orleans.GrainEx/Readme.txt
+++ NuGet/GameCloud.Orleans.GrainEx/Readme.txt
@@ -3,8 +3,9 @@
 ChangeLog
 
 -------------------------------------------------------------------------------
-Version: 1.00.000
+Version: 1.00.001
 Date: 2016.11.24
 
 a. 添加DCache
 b. 添加ClientWrapper
+c. 添加MongoClient
